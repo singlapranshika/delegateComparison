16b70ec14af9a408cb50d47bca3364c68e2a88c3
singlapranshika <44138859+singlapranshika@users.noreply.github.com>
2020-01-30 11:19:05 +0530

    Add files via upload

---
+++ Cities.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace delgateComparison
+{
+    class Cities
+    {
+       
+       
+        
+        public static int  CompareByName(string x,string y)
+        {
+            return x.CompareTo(y);
+
+        }
+        public static void Display(List<string> list)
+        {
+            foreach(string i in list)
+            {
+                Console.WriteLine(i);
+            }
+        }
+        static void Main(string[] args)
+        {
+            List<string> City = new List<string>();
+            City.Add("Hyderbad");
+            City.Add("delhi");
+            City.Add("shimla");
+            City.Add("chandigarh");
+            Display(City);
+            City.Sort(CompareByName);
+            Display(City);
+        }
+    }
+}
